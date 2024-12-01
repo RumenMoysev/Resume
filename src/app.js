6a49e10748f7436f491f0c686490c15887af191e
RumenMoysev <rumoysev@gmail.com>
2024-12-01 14:18:24 +0200

    Commit the whole resume

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const links = {
+    techReviewSite: 'https://github.com/RumenMoysev/Tech-Review-Site-Fullstack',
+    gallerySiteAngular: 'https://github.com/RumenMoysev/Gallery-Site-Fullstack',
+    gallerySiteVue: 'https://github.com/RumenMoysev/Gallery-site-VueJS',
+    jsWebDiploma: 'https://softuni.bg/certificates/details/215950/c976c785',
+    advancedCert: 'https://softuni.bg/certificates/details/174132/020ad251',
+    applicationsCert: 'https://softuni.bg/certificates/details/180044/300fc89f',
+    backEndCert: 'https://softuni.bg/certificates/details/190553/6fa38f63',
+    reactCert: 'https://softuni.bg/certificates/details/197834/96536f45',
+    htmlCssCert: 'https://softuni.bg/certificates/details/205107/88bebec2',
+    angularCert: 'https://softuni.bg/certificates/details/211745/a81a377c',
+    typescriptCert: 'https://softuni.bg/certificates/details/215539/2521d71f'
+}
+
+function openLink(link) {
+    window.open(links[link], '_blank')
+}
